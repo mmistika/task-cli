d3f73314ea17d9c6a55e5de4b65fffe50ad507e0
mmistika <mistikasoft@gmail.com>
2025-01-29 21:14:49 +0100

    feat: setup repository, manager and cmd handler in Main

--- src/main/java/io/github/mmistika/Main.java
+++ src/main/java/io/github/mmistika/Main.java
@@ -1,6 +1,19 @@
 package io.github.mmistika;
 
+import io.github.mmistika.cli.CommandHandler;
+import io.github.mmistika.domain.TaskManager;
+import io.github.mmistika.persistence.RepositoryException;
+import io.github.mmistika.persistence.TaskRepository;
+
 public class Main {
     public static void main(String[] args) {
+        try {
+            var repo = new TaskRepository();
+            var taskMgr = new TaskManager(repo);
+            var cmdHandler = new CommandHandler(taskMgr);
+            cmdHandler.handle(args);
+        } catch (RepositoryException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 }
